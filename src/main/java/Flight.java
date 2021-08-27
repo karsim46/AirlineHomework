b42e54d2e0f5af66193df5d44db7adf1e442f8eb
Simon <simonwatson176@gmail.com>
2021-08-27 18:12:14 +0100

    Plane Test added

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,27 @@
-public class Flight {
+import java.util.ArrayList;
+
+public class Flight extends Pilot{
+
+    private String flightNumber;
+    private String destination;
+    private String destinationAirport;
+    private String departureTime;
+    private ArrayList<String>passengers;
+    private ArrayList<Integer>cabinCrewMembers;
+
+
+
+    public Flight(String name, String rank, String pilotLicenseNumber, String flightNumber, String destination, String destinationAirport, String departureTime) {
+        super(name, rank, pilotLicenseNumber);
+        this.flightNumber = flightNumber;
+        this.destination = destination;
+        this.destinationAirport = destinationAirport;
+        this.departureTime = departureTime;
+        this.passengers = new ArrayList<>();
+        this.cabinCrewMembers = new ArrayList<>(5);
+
+
+    }
 }
+
+
