895b554d8d1d9268918f457ad512f10580f960ac
Simon <simonwatson176@gmail.com>
2021-08-27 23:02:58 +0100

    Pilot test added

---
+++ src/test/java/PilotTest.java
@@ -0,0 +1,28 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PilotTest {
+
+    Pilot pilot;
+
+    @Before
+    public void setUp(){
+        pilot = new Pilot("Bruce Dickinson", "Pilot", "IRMA666");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Bruce Dickinson", pilot.getName());
+    }
+    @Test
+    public void hasRank() {
+        assertEquals("Pilot", pilot.getRank());
+    }
+    @Test
+    public void hasPilotLicenseNumber(){
+        assertEquals("IRMA666", pilot.getPilotLicenseNumber());
+    }
+
+}
