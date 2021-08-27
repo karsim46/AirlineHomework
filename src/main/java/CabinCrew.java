203b5ea1170f6dd5720644d256b29d1beb5e3d51
Simon <simonwatson176@gmail.com>
2021-08-27 22:46:05 +0100

    Passenger bags added& tested

---
+++ src/main/java/CabinCrew.java
@@ -0,0 +1,22 @@
+public class CabinCrew {
+
+    private String name;
+    private String rank;
+
+    public CabinCrew(String name, String rank) {
+
+        this.name = name;
+        this.rank = rank;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getRank(){
+        return rank;
+    }
+
+
+}
+
