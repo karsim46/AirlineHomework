5807ecc188c5f37c0655c7a2c0fa20cd121fc999
Simon <simonwatson176@gmail.com>
2021-08-28 11:31:50 +0100

    Flight number/test added

--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -6,16 +6,24 @@ import static org.junit.Assert.assertEquals;
 public class FlightTest {
 
     Plane plane;
+    Flight flight;
+
 
     @Before
     public void setUp(){
         plane = new Plane(PlaneType.BOEING747,524,10000.00);
+        flight = new Flight(plane,"FL666");
     }
 
     @Test
     public void hasPlane(){
         assertEquals(PlaneType.BOEING747,plane.getType());
     }
+    @Test
+    public void hasFlightNumber(){
+        assertEquals("FL666",flight.getFlightNumber());
+    }
+
 
 
 }
